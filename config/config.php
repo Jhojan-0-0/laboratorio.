7e32eb0a1fcea80aebc0cf0137128d006bd785fd
josue <josuellatasichagua@gmail.com>
2024-05-30 10:42:45 -0500

    cambios

--- config/config.php
+++ config/config.php
@@ -4,9 +4,15 @@ define('URL', 'http://localhost/laboratorio/');
 
 define('COMPANY', 'laboratorio UNA-PUNO');
 define('HOST', 'localhost');
+<<<<<<< HEAD
 define('USER', 'jhojan');
 define('DB', 'katariPrice');
 define('PASSWORD', 'jhojan123');
+=======
+define('USER', 'josue');
+define('DB', '');
+define('PASSWORD', 'josue');
+>>>>>>> ed22630 (subiendo cambios de login)
 define('CHATSET', 'utf8mb4');
 
 date_default_timezone_set('America/Lima');
--- views/login/index.php
+++ views/login/index.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<<<<<<< HEAD
   <title><?php echo constant('COMPANY'); ?></title>
   <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/foundation/css/foundation.css">
   <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/foundation/css/foundation-float.css">
@@ -48,6 +49,34 @@
       </div>
     </div>
   </div>
+=======
+  <title>Login Page</title>
+  <link rel="stylesheet" href="./public/css/login.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  
+</head>
+
+<body>
+  <div class="login-container">
+    <div class="login-form">
+      <h2>Login</h2>
+      <form action="login/logIn" method="POST">
+        <label for="usuario">Usuario</label>
+        <input type="text" name="usuario" id="usuario" required>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" required>
+        <a href="#" style="color: #28a745; margin-bottom: 20px;">     </a>
+        <input type="submit" value="SIGN IN">
+      </form>
+      <img src="https://www.eduopinions.com/wp-content/uploads/2017/11/Universidad-Nacional-del-Altiplano-UNAP-logo.jpg" alt="Logo" style="margin-top: 20px; width: 100px;">
+    </div>
+    <div class="image-section"></div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js"></script>
+>>>>>>> ed22630 (subiendo cambios de login)
 </body>
 
 </html>
