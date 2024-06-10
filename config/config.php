336434453ee1a8e1cea3a3a129ca783ef3c32d5e
Elmer0200 <qelmergabriel@gmail.com>
2024-06-09 22:29:24 -0500

    bajando cambios

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ define('COMPANY', 'Laboratorio UNA-PUNO');
 define('HOST', 'localhost');
 define('USER', 'loquinho');
 define('DB', 'laboratorio');
-define('PASSWORD', '123');
+define('PASSWORD', '123456');
 define('CHATSET', 'utf8');
 
 date_default_timezone_set('America/Lima');
