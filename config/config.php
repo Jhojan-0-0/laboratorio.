a79853561ba3211135c86b793774a3b38cf6e4f4
josue_llatasi <josuellatasichagua@gmail.com>
2024-06-24 21:50:40 -0500

    nuevas vistas de la pagina de home

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@ define('URL', 'http://localhost/laboratorio/');
 
 define('COMPANY', 'Laboratorio UNA-PUNO');
 define('HOST', 'localhost');
-define('USER', 'jhojan');
-define('DB', 'laboratorioUNA');
-define('PASSWORD', 'jhojan123');
+define('USER', 'josue');
+define('DB', 'laboratorio');
+define('PASSWORD', 'josue');
 define('CHATSET', 'utf8');
 
 date_default_timezone_set('America/Lima');
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,11 +1,36 @@
 <?php require ('views/header.php');?>
 
-<div class="grid-container">
-	<div class="grid-x">
-		<div class="cell">
-			<h3>Bienvenidos</h3>
-		</div>
-	</div>
-</div>
+<link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/home.css">
+
+
+    <div class="grid-container">
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12 medium-6">
+                <img src="LogoUNAP.png" alt="Logo UNAP">
+            </div>
+            <div class="cell small-12 medium-6">
+                <div class="callout secondary">
+                    <h1>Bienvenido Al Mega Laboratorio UNAP</h1>
+                </div>
+            </div>
+        </div>
+<br>	
+<br>
+        <div class="grid-x grid-margin-x">
+            <div class="cell small-12 medium-6">
+                <h2>Misión</h2>
+                <p>Desarrollar y transmitir el conocimiento científico, tecnológico y humanístico, formando 
+					profesionales y posgraduados calificados y competitivos, impulsando el desarrollo de la investigación 
+					y la responsabilidad social, la práctica de valores y la identidad cultural, orientadas al 
+					estudiante y la sociedad, con miras a contribuir al desarrollo sostenible de la región y del país.</p>
+            </div>
+            <div class="cell small-12 medium-6">
+                <h2>Visión</h2>
+                <p>Formar profesionales y posgraduados calificados y competitivos; aportando a la sociedad los 
+					resultados de la investigación científica, tecnológica y humanística, con identidad cultural 
+					y responsabilidad social, que contribuyan al desarrollo sostenible de la región y del país</p>
+            </div>
+        </div>
+    </div>
 
 <?php require ('views/footer.php');?>
